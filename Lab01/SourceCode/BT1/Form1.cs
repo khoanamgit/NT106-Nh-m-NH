ff8d2c15098c0444fb21c0858d90d406f31a5969
khoanamgit <75235152+khoanamgit@users.noreply.github.com>
2022-03-07 16:24:04 +0700

    Add files via upload

---
+++ Lab01/SourceCode/BT1/Form1.cs
@@ -0,0 +1,57 @@
+﻿namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // Kiểm tra đầu vào
+            try
+            {
+                // Lấy dữ liệu từ 2 textbox
+                string first_num = textBox1.Text;
+                string second_num = textBox2.Text;
+
+
+                // Ép kiểu dữ liệu vừa mới lấy
+                int a = Int32.Parse(first_num);
+                int b = Int32.Parse(second_num);
+
+
+                // Tính tổng 2 số lưu vào biến kết quả
+                long result = a + b;
+
+                textBox3.Text = result.ToString();  
+                
+               
+            }
+
+            catch 
+            {
+                MessageBox.Show("Vui lòng nhập số nguyên");
+
+            }   
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
