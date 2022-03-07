1cea7043248e12330959ae570a834f9137ea2e9d
khoanamgit <75235152+khoanamgit@users.noreply.github.com>
2022-03-07 16:25:55 +0700

    Add files via upload

---
+++ Lab01/SourceCode/BT3/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string first_num = textBox1.Text;
+            
+            int  a = Int32.Parse(first_num);
+
+            switch(a)
+            {
+                case 0:
+                    textBox2.Text = "Không";
+                    break;
+                case 1:
+                    textBox2.Text = "Một";
+                    break;
+                case 2:
+                    textBox2.Text = "Hai";
+                    break;
+                case 3:
+                    textBox2.Text = "Ba";
+                    break;
+                case 4:
+                    textBox2.Text = "Bốn";
+                    break;
+                case 5:
+                    textBox2.Text = "Năm";
+                    break;
+                case 6:
+                    textBox2.Text = "Sáu";
+                    break;
+                case 7:
+                    textBox2.Text = "Bảy";
+                    break;
+                case 8:
+                    textBox2.Text = "Tám";
+                    break;
+                case 9:
+                    textBox2.Text = "Chín";
+                    break;
+            }    
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = String.Empty;
+            textBox2.Text = String.Empty;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bạn có muốn thoát không");
+            Application.Exit(); 
+        }
+    }
+}
