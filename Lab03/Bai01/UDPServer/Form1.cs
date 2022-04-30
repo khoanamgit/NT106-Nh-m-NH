576bf9d9ed635944542ee53c224cc4bb3ac095e3
khoanamgit <75235152+khoanamgit@users.noreply.github.com>
2022-04-30 23:37:42 +0700

    Add files via upload

---
+++ Lab03/Bai01/UDPServer/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UDPServer
+{
+    public partial class Form1 : Form
+    {
+        public delegate void ShowMessage(string message);
+        public ShowMessage myDelegate;
+        Int32 port = 8080;
+        UdpClient udpClient = new UdpClient(8080);
+        Thread thread;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+      
+            richTextBox1.Text = "Start Listening" + "\n";
+            myDelegate = new ShowMessage(Show);
+            thread = new Thread(new ThreadStart(Received));
+            thread.IsBackground = true;
+            thread.Start();
+        }
+        public void Received()
+        {
+            while (true)
+            {
+                IPEndPoint RemoteIPEndPoint = new IPEndPoint(IPAddress.Any, port);
+                byte[] data = udpClient.Receive(ref RemoteIPEndPoint);
+                if (data.Length > 0)
+                {
+                    string message = RemoteIPEndPoint.Address.ToString() + ": " + UTF32Encoding.UTF32.GetString(data);
+                    this.Invoke(myDelegate, new object[] { message });
+                }
+            }
+        }
+
+        private void Show(string message)
+        {
+            richTextBox1.Text += message + "\n";
+        }
+    }
+}
