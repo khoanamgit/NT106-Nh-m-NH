768bb6388c57e0d7832429ac5d5294424cf6282a
khoanamgit <75235152+khoanamgit@users.noreply.github.com>
2022-04-30 23:32:14 +0700

    Add files via upload

---
+++ Lab04/Bai01/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox1.Text = getHTML(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("URL không tồn tại!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox1.Text = "http://";
+            }
+        }
+
+        private string getHTML(string szURL)
+        {
+            // Create a request for the URL.
+            WebRequest request = WebRequest.Create(szURL);
+            // Get the response.
+            WebResponse response = request.GetResponse();
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            // Close the response.
+            response.Close();
+            return responseFromServer;
+        }
+
+    }
+}
